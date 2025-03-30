29648835c4bff7df1f659937b5e7713e9f472527
Xiaoqi Xie <xiaoqi.xie@mail.mcgill.ca>
2025-03-30 16:38:49 -0400

    Update Data_simulation_helper.py

--- Data_simulation_helper.py
+++ Data_simulation_helper.py
@@ -21,77 +21,53 @@ def sites_samples(sampling_type, N, I):
     return n_samples
 
 
-def age_sex_simulation(sampling_type,age_type,n_samples,I):#for sampling type being homo, age tpye must be homo, for heteroneity, we have homo and inhomo for age type
+def age_sex_simulation(sex_type,age_type,n_samples):#for sampling type being homo, age tpye must be homo, for heteroneity, we have homo and inhomo for age type
     age=[]
     sex=[]
-    # n_samples=sites_samples(sampling_type, N, I)
-    if sampling_type=='Homogeneous' and age_type=='Homogeneous':
+    
+    if sex_type=="Homogenous":
         for n in n_samples:
-            #covariates sex
-            #for site i with n_i samples, the probability of male is equal to female as 50%.
             v1=np.random.binomial(n=1, p=0.5, size=n)
             sex.append(v1)
+    else:
+        p=[np.random.uniform(0.2,0.8,1) for _ in range(len(n_samples))]#each site has a unique sex distribution
+        for i,n in enumerate(n_samples):
+            v1=np.random.binomial(n=1, p=p[i], size=n)
+            sex.append(v1)
+    if age_type=='Homogeneous':
+        mu=np.random.uniform(20,65,1)
+        a=mu/3-5
+        b=mu/3+5
+        sd=float(np.random.uniform(a, b, 1))
+        for i, n in enumerate(n_samples):
             #covariate age
             #by reading the median of all boxplots, I guess the mean to be 40 and 10 for sd.
-            v2=stats.norm.rvs(loc=50, scale=15, size=n)
+            v2=stats.norm.rvs(loc=mu, scale=sd, size=n)
             v2 = v2[v2 > 0]
             # print(len(v2))
             while len(v2) < n:
-                additional_samples = stats.norm.rvs(loc=50, scale=15, size=n - len(v2))
+                additional_samples = stats.norm.rvs(loc=mu, scale=sd, size=n - len(v2))
                 v2 = np.concatenate((v2, additional_samples[additional_samples > 0]))
             age.append(v2)
-    elif sampling_type == 'Heterogeneity':
-        if age_type=='Homogeneous':
-            p=[np.random.uniform(0.2,0.8,1) for _ in range(len(n_samples))]#each site has a unique sex distribution
-            # print('p:',p)
-            mu=np.random.uniform(20,65,1)
+    else:
+        mu_age=np.random.uniform(20,65,len(n_samples))
+        for i, n in enumerate(n_samples):
+            #covariate age
+            mu=mu_age[i]
             a=mu/3-5
             b=mu/3+5
             sd=float(np.random.uniform(a, b, 1))
-
-            for i, n in enumerate(n_samples):
-                print('i,n:',i,n)
-                #covariates sex
-                v1=np.random.binomial(n=1, p=p[i], size=n)
-                sex.append(v1)
-                #covariate age
-                #by reading the median of all boxplots, I guess the mean to be 40 and 10 for sd.
-                v2=stats.norm.rvs(loc=mu, scale=sd, size=n)
-                v2 = v2[v2 > 0]
-                # print(len(v2))
-                while len(v2) < n:
-                    additional_samples = stats.norm.rvs(loc=mu, scale=sd, size=n - len(v2))
-                    v2 = np.concatenate((v2, additional_samples[additional_samples > 0]))
-                age.append(v2)
-
-        if age_type=='Heterogeneity':
-            p=[np.random.uniform(0.2,0.8,1) for _ in range(len(n_samples))]#each site has a unique sex distribution
-            # print('p:',p)
-            mu_age=np.random.uniform(20,65,I)
-
-            for i, n in enumerate(n_samples):
-                print('i,n:',i,n)
-                #covariates sex
-                v1=np.random.binomial(n=1, p=p[i], size=n)
-                sex.append(v1)
-                #covariate age
-                mu=mu_age[i]
-                a=mu/3-5
-                b=mu/3+5
-                sd=float(np.random.uniform(a, b, 1))
-                #by reading the median of all boxplots, I guess the mean to be 40 and 10 for sd.
-                v2=stats.norm.rvs(loc=mu, scale=sd, size=n)
-                v2 = v2[v2 > 0]
-                # print(len(v2))
-                while len(v2) < n:
-                    additional_samples = stats.norm.rvs(loc=mu, scale=sd, size=n - len(v2))
-                    v2 = np.concatenate((v2, additional_samples[additional_samples > 0]))
-                age.append(v2)
+            #by reading the median of all boxplots, I guess the mean to be 40 and 10 for sd.
+            v2=stats.norm.rvs(loc=mu, scale=sd, size=n)
+            v2 = v2[v2 > 0]
+            # print(len(v2))
+            while len(v2) < n:
+                additional_samples = stats.norm.rvs(loc=mu, scale=sd, size=n - len(v2))
+                v2 = np.concatenate((v2, additional_samples[additional_samples > 0]))
+            age.append(v2)
             
     return age,sex
 
-# def smooth_leaky_relu(x, alpha=0.9, beta=10):
-#     return alpha * torch.log(1+ torch.exp(beta * x)) / beta
 
 def u_shape(x, a=1, b=0, c=0):
     return a * (x - b) ** 2 + c
@@ -119,15 +95,7 @@ def fixed_effect_nonlinear(x_df):#control this to have at most on modal
             nn.init.normal_(self.output_layer.bias, mean=0, std=1)
         def custom_leaky_relu(self, x):
             """Custom Leaky ReLU: Uses log(1 + random_val) when x == 0"""
-            # random_val = torch.rand_like(x) * 0.5  # Random value in (0, 0.5)
-            # log_value = torch.log(1 + random_val)  # Compute log(1 + a)
             epsilon=0
-            # a = 2
-            # b =0
-            # c = 0
-            # quadratic_part = a * x**2 + b * x + c  # Parabolic smoothing
-
-            # negative_slope=-1
             leaky_relu_part = torch.where(x >epsilon, x, -x)
 
 
@@ -135,18 +103,10 @@ def fixed_effect_nonlinear(x_df):#control this to have at most on modal
 
            
         def forward(self, x):
-            """make a gausian plot"""
-            # leaky_relu=nn.LeakyReLU(negative_slope=-1)
-            # elu = nn.ELU(alpha=1.0)
             h = torch.sigmoid(self.hidden_layer1(x))
-            #torch.tanh(self.hidden_layer1(x))
-            #leaky_relu(self.hidden_layer1(x))
-            # h = torch.relu(self.hidden_layer2(h))
-            # h = torch.relu(self.hidden_layer3(h))
-            out = 10*(np.sin(2*np.pi*self.output_layer(h)/10))
+            out = 10*(np.sin(2*np.pi*self.output_layer(h)/10))#transformation done here for ensuring non-negative
             return (out)
 
-    # torch.manual_seed(549)
     global_model = MLP(input_dim=2, hidden_dim=128, output_dim=1)
     global_model.eval()
     X_tensor = torch.tensor(x_df.values, dtype=torch.float32)
@@ -163,3 +123,8 @@ def fixed_effect(x_df,effect_type):
         theta_g=(stats.norm.rvs(loc=0,scale=1,size=2))
         phi_g=np.dot(x_df,theta_g)
         return(phi_g)
+
+            
+
+
+    
